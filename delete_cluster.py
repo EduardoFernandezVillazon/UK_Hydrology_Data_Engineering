c67d382806417453c05e99e1701c8c3087640f37
EduardoFernandezVillazon <eduardofernandezvillazon4@gmail.com>
2020-09-03 18:55:05 +0100

    delete instances function with try except module added

--- delete_cluster.py
+++ delete_cluster.py
@@ -16,17 +16,12 @@ def delete_keypair():
 ec2 = boto3.client('ec2',
                    region_name="us-west-2",
                    )
-def delete_instances():
-    try:
-        with open('instance_ids.csv', mode='r') as instance_id_file:
-            instance_reader_rows = csv.reader(instance_id_file, delimiter=',', quotechar='"')
-            for row in instance_reader_rows:
-                response = ec2.terminate_instances(InstanceIds=row)
 
-        os.system('rm ~/PycharmProjects/instance_ids.csv')
-    except:
-        pass
+with open('instance_ids.csv', mode='r') as instance_id_file:
+    instance_reader_rows = csv.reader(instance_id_file, delimiter=',', quotechar='"')
+    for row in instance_reader_rows:
+        response = ec2.terminate_instances(InstanceIds=row)
 
+os.system('rm ~/PycharmProjects/instance_ids.csv')
 
 delete_keypair()
-delete_instances()
